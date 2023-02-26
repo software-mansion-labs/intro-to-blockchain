23d1dc31c47f2d911e834445214f176ac5c77574
Maksymilian Demitraszek <maksymilian.demitraszek@protonmail.com>
2023-02-26 12:44:10 +0100

    Updated tests

--- exercise1/test_exercises.py
+++ exercise1/test_exercises.py
@@ -1,5 +1,7 @@
 from exercise1.hash_1 import Transaction
 from exercise1.public_key_2 import Alice, Bob
+from exercise1 import signature_3
+from simple_cryptography import generate_key_pair, sign
 
 def test_hash():
     assert Transaction(1, 2, "xDDDDDD").hash() == b"'sP[B9\x99\xec\xc1`\xf7\xcb?\xe9\xf6R\xf8\xe3v\x1f4\t\xecX%\xec\xc0\x0f{\xad\x0fN"
@@ -16,3 +18,18 @@ def test_encryption():
     decrypted = bob.decrypt(encrypted)
     assert secrect_message == decrypted
     
+
+def test_signature():
+    alice = signature_3.Alice()
+    bob = signature_3.Bob(alice)
+    signature = alice.sign()
+    decrypted = bob.validate_signature(signature)
+    assert secrect_message == decrypted
+
+
+def test_malicious_alice():
+    alice = signature_3.Alice()
+    bob = signature_3.Bob(alice)
+    (_, priv) = generate_key_pair()
+    somebody_else_signed = sign(priv, bytes(signature_3.UMOWA, "utf-8"))
+    assert not bob.validate_signature(somebody_else_signed)
