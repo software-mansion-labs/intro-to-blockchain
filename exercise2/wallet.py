79315986fbe7cc0c3eb632a24a719ac7032edb61
Bartosz Rybarski <bartosz.rybarski@swmansion.com>
2023-03-09 17:36:40 +0100

    Added SignedTransaction type

--- exercise2/wallet.py
+++ exercise2/wallet.py
@@ -1,18 +1,18 @@
-import argparse
-import requests
 from simple_cryptography import PrivateKey, PublicKey, sign, generate_key_pair
-from exercise2.transaction_registry import TransactionRegistry, Transaction
-from typing import Optional, Tuple
+from exercise2.transaction_registry import TransactionRegistry, Transaction, SignedTransaction
+from typing import Optional, Tuple, List
 
 class Wallet:
     public_key: PublicKey
-    _private_key: PrivateKey
 
     def __init__(self, key_pair: Tuple[PublicKey, PrivateKey]):
         self.public_key = key_pair[0]
+
+        # W produkcyjnych warunkach należy szczególnie zadbać o bezpieczeństwo klucza prywatnego.
+        # W przypadku naszych warsztatów nie musimy się tym przejmować.
         self._private_key = key_pair[1]
 
-    def get_unspent_transactions(self, registry: TransactionRegistry) -> list[Transaction]:
+    def get_unspent_transactions(self, registry: TransactionRegistry) -> List[Transaction]:
         """
         TODO: Znajdź wszystkie niewykorzystane transakcje powiązane z tym portfelem.
         Spośród wszystkich transakcji w rejestrze (registry.transactions), zwróć te z których
@@ -31,12 +31,21 @@ class Wallet:
         """
         return len(self.get_unspent_transactions(registry))
 
+    def sign_transaction(self, transaction: Transaction) -> SignedTransaction:
+        """
+        TODO: Podpisz transakcję kluczem prywatnym.
+        Korzystając z funkcji sign z simple_cryptography, stwórz podpis danej transakcji.
+        Następnie zwróc podpisaną transakcję jako obiekt klasy SignedTransaction.
+        """
+        signature = sign(self._private_key, transaction.tx_hash)
+        return SignedTransaction(transaction, signature)
+
     def transfer(self, registry: TransactionRegistry, recipient: PublicKey) -> bool:
         """
         TODO: Przekaż coina do nowego właściciela.
         - Znajdź dowolną niewykorzystaną transakcję, jeśli takiej nie ma, zwróć False.
         - Stwórz nową transakcję, z podanym odbiorcą (recipient) oraz poprzednim hashem znalezionej transakcji.
-        - Podpisz nową transakcję kluczem prywatnym portfela.
+        - Podpisz nową transakcję korzystając z sign_transaction.
         - Dodaj transakcję do rejestru.
         - Zwróć True jeśli wszystko się udało, False w przeciwnym wypadku.
         """
@@ -46,6 +55,6 @@ class Wallet:
             return False
 
         new_transaction = Transaction(recipient, unspent_transactions[0].tx_hash)
-        new_transaction.signature = sign(self._private_key, new_transaction.tx_hash)
+        signed_transaction = self.sign_transaction(new_transaction)
 
-        return registry.add_transaction(new_transaction)
+        return registry.add_transaction(signed_transaction)
