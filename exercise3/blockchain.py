41eb29390691ad4182acc441f1856a00b3ff659a
war-in <marcin.warchol20@gmail.com>
2023-03-10 13:25:44 +0100

    remove code

--- exercise3/blockchain.py
+++ exercise3/blockchain.py
@@ -18,32 +18,22 @@ class Blockchain:
         """
         TODO: Zwróć ostatni blok.
         """
-        return self.blocks[-1]
+        raise NotImplementedError()
 
     def length(self) -> int:
         """
         TODO: Zwróć długość łańcucha.
         """
-        return len(self.blocks)
+        raise NotImplementedError()
 
     def get_transaction(self, tx_hash: bytes) -> Optional[Transaction]:
         """
         TODO: Przy pomocy podanego `tx_hash` wyszukaj transakcję.
         """
-        for block in self.blocks:
-            for transaction in block.transactions:
-                if transaction.tx_hash == tx_hash:
-                    return transaction
-
-        return None
+        raise NotImplementedError()
 
     def get_transaction_by_previous_tx_hash(self, previous_tx_hash: bytes) -> Optional[Transaction]:
         """
         TODO: Przy pomocy podanego `previous_tx_hash` wyszukaj transakcję.
         """
-        for block in self.blocks:
-            for transaction in block.transactions:
-                if transaction.previous_tx_hash == previous_tx_hash:
-                    return transaction
-
-        return None
+        raise NotImplementedError()
